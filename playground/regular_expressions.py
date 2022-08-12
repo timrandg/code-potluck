06d6320dc6cdae463ae1ae69f2b2af4814889a19
Maxwell Vaglica <maxwell.vaglica@tmps-nb-4048.local>
2022-08-12 13:00:09 -0500

    regex stuff

--- playground/regular_expressions.py
+++ playground/regular_expressions.py
@@ -28,8 +28,32 @@ to find phone numbers in the following three sentences s1, s2, s3.
 '''
 import regex as re
 # Write your code here:
+#re.search(regular_expression, string)
+phone_num =  r'\d{3}-\d{3}-\d{4}'
+s1 = "This sentence contains a phone number (415-630-2001)."
+assert re.search(phone_num, s1)[0] == '415-630-2001'
 
-# Do not edit any code below this line!
+assert re.match(phone_num, s1) == None
+## This regular expression does not match s1 because s1 does not BEGIN with a phone number
+
+assert re.findall(phone_num, s1) == ['415-630-2001']
+## re.findall() will find all cases in a string that match the regular expression
+
+s3 = "415-630-2119 This sentence contains a two phone numbers (cell: 415-630-2001)."
+assert re.search(phone_num, s3)[0] == '415-630-2119'
+
+assert re.match(phone_num, s3)[0] == '415-630-2119'
+
+assert re.findall(phone_num, s3) == ['415-630-2119', '415-630-2001']
+
+##What about for dates?
+date_re = r'\d{4}-\d{2}-\d{2}' ##What is the regular expression for this?
+
+date = '2022-08-12'
+
+assert re.match(date_re,date)
+
+#Do not edit any code below this line!
 if __name__ == '__main__':
     import doctest
     count, _ = doctest.testmod()
